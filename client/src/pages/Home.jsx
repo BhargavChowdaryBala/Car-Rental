10fe9c7ae72c20b9de44f61111d73924cd9e0ec0
bhargav <bhargavbala56@gmail.com>
2026-05-30 01:10:11 +0530

    Developed TestMonial (user feedback) frontedn part successfully

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import FeaturedSection from '../components/FeaturedSection';
+import Banner from '../components/Banner';
+import TestMonial from '../components/TestMonial';
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedSection />
+      <Banner />
+      <TestMonial />
+
     </>
   )
 }
